8a9dfb0436f44bbf6c0b4c98ef85df68621275a3
VadimMarenko <130446385+VadimMarenko@users.noreply.github.com>
2023-11-11 16:02:24 +0200

    Update schemas.py
    add UserResponse and TokenModel classes

--- src/schemas.py
+++ src/schemas.py
@@ -6,7 +6,9 @@ from pydantic import BaseModel, EmailStr, Field, condate
 class UserModel(BaseModel):
     first_name: str = Field(default="", max_length=25)
     last_name: str = Field(default="", max_length=30)
-    email: EmailStr | None
+    username: str = Field(min_length=6, max_length=12)
+    email: EmailStr
+    password: str = Field(min_length=6, max_length=8)
     phone_number: str = Field(default="", max_length=25)
     born_date: date | None
     description: str = Field(default="", max_length=250)
@@ -27,3 +29,19 @@ class ResponseUser(BaseModel):
 
 class UserEmailModel(BaseModel):
     email: EmailStr
+
+
+class UserResponse(BaseModel):
+    id: int
+    username: str
+    email: str
+    avatar: str
+
+    class Config:
+        from_attributes = True
+
+
+class TokenModel(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str = "bearer"
